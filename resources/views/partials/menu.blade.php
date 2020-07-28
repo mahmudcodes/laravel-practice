8f60e5e42625046aee235a96ef6b3271bb7f8253
Mahmud Hasan <mahmudhasan053@gmail.com>
2020-07-28 18:42:41 +0600

    person menu change

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -1,4 +1,26 @@
-<a href="{{ route('homepage') }}">Home</a>
-<a href="{{ route('aboutpage') }}">About</a>
-<a href="{{ route('contactpage') }}">Contact</a>
-<a href="{{ route('extrapage') }}">Extra</a>
+<nav class="navbar navbar-expand-lg navbar-light bg-light mt-5 mb-5">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <a class="navbar-brand" href="{{ route('homepage') }}">Home</a>
+      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <li class="nav-item active">
+          <a class="nav-link" href="{{ route('aboutpage') }}">About <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{ route('contactpage') }}">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{ route('extrapage') }}">Extra</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('personPage') }}">Person</a>
+          </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" id="myInput" onkeyup="myFunction()">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
--- resources/views/person.blade.php
+++ resources/views/person.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 @section('title','Person - Details')
 @section('content')
-<table class="table table-bordered table-hover table-striped">
+<table class="table table-bordered table-hover table-striped mt-5 mb-5" id="myTable">
     <thead>
       <tr>
         <th scope="col">#</th>
@@ -21,4 +21,30 @@
         @endforeach
     </tbody>
   </table>
+@endsection
+
+@section('script')
+    <script>
+        function myFunction() {
+        // Declare variables
+        var input, filter, table, tr, td, i, txtValue;
+        input = document.getElementById("myInput");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("myTable");
+        tr = table.getElementsByTagName("tr");
+        
+        // Loop through all table rows, and hide those who don't match the search query
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[0];
+            if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+            }
+        }
+        }
+    </script>
 @endsection
